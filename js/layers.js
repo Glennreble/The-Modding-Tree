29b3291631840b60f64a06e59e0a8fbfcf8232aa
Secret1000 <141339442+Glennreble@users.noreply.github.com>
2023-12-02 11:56:21 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("l", {
     baseResource: "XP", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.3, // Prestige currency exponent
+    exponent: 1.2, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -39,6 +39,11 @@ addLayer("l", {
 	title: "XP Converter",
 	description: "x5 more xp.",
 	cost: new Decimal (3),
+	},
+	13: {
+	title: "XP Calculator",
+	description: "x10 more xp.",
+	cost: new Decimal (7),
 	}
     },
 })
