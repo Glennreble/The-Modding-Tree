43279a804f2de6be3a13d8f990e16abe0d24303a
Secret1000 <141339442+Glennreble@users.noreply.github.com>
2023-12-04 10:26:54 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -44,6 +44,48 @@ addLayer("l", {
 	title: "XP Calculator",
 	description: "x10 more xp.",
 	cost: new Decimal (7),
+	},
+        14: {
+	title: "XP Booster",
+	description: "x50 more xp, unlock a new layer.",
+	cost: new Decimal (8)
 	}
     },
+})
+addLayer("t", {
+    name: "tiers", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FFFF00",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "tiers", // Name of prestige currency
+    baseResource: "levels", // Name of resource prestige is based on
+    baseAmount() {return player.l.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade("l",14)||return player.t.unlocked},
+    upgrades: {
+        11: {
+        title: "Tier to XP",
+        description: "Tiers boost your xp gain.",
+        cost: new Decimal(1),
+        effect() {
+            return player[this.layer].points.add(1).pow(0.8)
+        },
+    }
 })
